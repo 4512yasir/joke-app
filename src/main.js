92416376d15e9c84a3491be573261ca050a575ce
4512yasir <yasiryaska1@gmail.com>
2025-03-27 16:51:43 +0300

    fully written readme.md

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    
-    const input = document.getElementById("joke-genre");
+   const input = document.getElementById("joke-genre");
     const searchButton = document.getElementById("search");
     const display = document.getElementById("display");
     const addJokeBtn = document.getElementById("add-joke-btn");
